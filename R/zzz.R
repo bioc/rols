da27ab6cf0ea530cdef967bc2fc75a2a1f9edf9e
lshep <lori.shepherd@roswellpark.org>
2026-03-30 07:35:31 -0400

    maintainer requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,8 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(paste("\nThis is 'rols' version",
                                 packageVersion("rols"), "\n"))
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.24")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
